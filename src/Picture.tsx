4f023ec3a568a822a66f574165011f410a297537
Daniel Kalman <daniel.kalman@quarters.com>
2021-03-31 17:06:40 +0200

    xyz

--- src/Picture.tsx
+++ src/Picture.tsx
@@ -1,4 +1,4 @@
-import { makeStyles, ThemeProvider, Typography } from "@material-ui/core";
+import { ThemeProvider, Typography } from "@material-ui/core";
 import React from "react";
 import { themeOne as theme } from "./theme";
 
